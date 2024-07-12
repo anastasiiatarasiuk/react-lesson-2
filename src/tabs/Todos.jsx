7eea6ba0a624ca63a7326e5b8db3b73d98140af6
InD1909 <ilyado1909@gmail.com>
2024-07-12 20:51:46 +0300

    Todo render

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,20 @@
-import { Text } from 'components';
+import { Form, Text } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+  const todoAdd = text => {
+    const newTodo = {
+      id: nanoid(),
+      text: text,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  return (
+    <>
+      <Form onSubmit={todoAdd} />
+      {/* <Text textAlign="center">There are no any todos ...</Text> */}
+    </>
+  );
 };
